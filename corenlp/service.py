5194d682c6e86b4ff6c9a22b6548875f9245f17a
twneale <twneale@gmail.com>
2015-04-19 13:24:01 -0400

    Parameterize the connection info

--- corenlp/service.py
+++ corenlp/service.py
@@ -1,17 +1,21 @@
-import sys
 import zmq
+
+from corenlp.models import CoreNLP
 
 
 class Service:
     default_annotators = 'tokenize,ssplit,pos,lemma,parse'
 
+    self._broker_host = os.environ['CORENLP_BROKER_HOST']
+    self._broker_port = os.environ['CORENLP_BROKER_PORT']
+
     def __init__(self):
         context = zmq.Context()
-        socket = context.socket(zmq.REQ)
-        socket.connect("tcp://localhost:5559")
+        self.socket = context.socket(zmq.REQ)
+        self.socket.connect("tcp://localhost:5559")
 
     def send(self, string, annotators=None):
         obj = dict(annotators=self.default_annotators, text=string)
-        socket.send_json(obj)
-        message = socket.recv_json()
-        return message
+        self.socket.send_json(obj)
+        message = self.socket.recv_json()
+        return CoreNLP(message)
