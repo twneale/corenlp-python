43b5b9b8a61c47e112b1386b4abe695dcf846499
twneale <twneale@gmail.com>
2015-04-19 13:29:30 -0400

    Default cnxn params

--- corenlp/service.py
+++ corenlp/service.py
@@ -8,13 +8,12 @@ from corenlp.models import CoreNLP
 class Service:
     default_annotators = 'tokenize,ssplit,pos,lemma,parse'
 
-    self._broker_host = os.environ['CORENLP_BROKER_HOST']
-    self._broker_port = os.environ['CORENLP_BROKER_PORT']
-
     def __init__(self):
+        self._broker_host = os.environ.get('CORENLP_BROKER_HOST', 'localhost')
+        self._broker_port = os.environ.get('CORENLP_BROKER_PORT', '5559')
         context = zmq.Context()
         self.socket = context.socket(zmq.REQ)
-        self.socket.connect("tcp://localhost:5559")
+        self.socket.connect("tcp://%s:%s" % (self._broker_host, self._broker_port))
 
     def send(self, string, annotators=None):
         obj = dict(annotators=self.default_annotators, text=string)
